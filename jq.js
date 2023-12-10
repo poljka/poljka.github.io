2c523d19515ab5102aa161bcc27f599eecdf0840
poljka <149972019+poljka@users.noreply.github.com>
2023-12-11 04:10:21 +0800

    Create jq.js

---
+++ jq.js
@@ -0,0 +1,40 @@
+var aa = [
+   	
+'https://qm.qq.com/cgi-bin/qm/qr?k=TEfUSH9AsAVzdmvdLM6Qw5heJabysW6l&authKey=Tyw+WzTOp0HWADASuIPPVFf/Nwq1u2FxQQlFFzpVt8RmM+EyfLNdQcqVvv6Okems&noverify=0',//狼菊惩百肚-13
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=2HNmGf9XB-fbdkPagzrMSru_vovKbp0C&authKey=C5mYKFB7mzv1tG+9Ug6tbz+j3ZZqXzYFZInXXJg2djtyIwRj5QLQ7XGGj5iidSKP&noverify=0',//巨充次领微-15
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=iWsc8OfiZdjKRIZHybRR98mnL-VL1nQH&authKey=H5Fn1g1J9ml7ZvoV/okOdYiWJIZraBSS487tPvTk8QNfq5MCRES0IcoaFwwAvF44&noverify=0',//畏漏胀买咳-11
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=RyxWqP_PSrnfcn8cGsxtF-VpsU_B-GUG&authKey=GET0SCWdt9JUHTgn+6bldhIlSc109x75a9kC3TRn4Iriys+aIQPkVgKdge+YR7Kn&noverify=0',//践够啦怠知-3
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=m4mYgmfR7m45B0Ve5JWT3BW8JUMzzZL0&authKey=QgcE3WbFRxZj9jE9ugGt+dZaiwJhjvBq3DUW/Q2DpnFxtIv7SJvhD7wrVu8kTpD+&noverify=0',//航织尾仓迁-7
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=cwm8SjgZM0ahMEfX4ZMgeYDwQZIdpqYw&authKey=/b5kyJApun3yTepm2mafjKwYYkybpRuI1RQB6NWS1tWLJ5YTqZc9ZBommY+trE4m&noverify=0',//梢灰需库束-6
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=eBxr34gFixYWdmoifSQD5JI4Ii1A-5hX&authKey=rOgFjIZWsF47yF2sh7BsSFCnYJ9diJdPWvmqKED6qKRxUOiedFIPj4eowJfRnAW4&noverify=0',//弟唐规淡初-5
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=wxixpM_l9EPIj2OVGBkE95NEXnLdOOlX&authKey=gzgTedyo6zBzHyL6sXOkrMM7Qbqcr9yP6WPlqK2EwewmuPkmgZze7ueW6wgkPV2r&noverify=0',//授藏耍孤润-4
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=K_L_eKkcGtU2hmqvaLc6rp_XzVJezLAd&authKey=azS8X+nwi8TlD+NDYxDIKoDa9RtGrISvtjMNQBIw7Ryo2gpcvMt9b4kwOyBVyt8q&noverify=0',//裂含毁变蓄-1
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=tC98NcRTZMWkvfBPmZpibheRSo_BrT7Y&authKey=pVAlp4jQzCXb2pZNbX/6ta0ey13SCio4mvnea1Rl34BZAl0IjOkaVFR4j0PtKkxZ&noverify=0',//晒拐州塞赢-10
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=6Y6HmvPxdrXjkM-lbh1WEGmu7j4l82eS&authKey=H8ZuRN4K6U01IJXzvb2IiaW6Na/kFTS0pwCynEmAVxuTAvS2NAEcNxG3Ojf4Qxt8&noverify=0',//没桥谎淋愚-2
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=hiNQIlteKTzqXaqlSvtW4DHjTKwfERwv&authKey=PKWpPkEPG4MQJ0mtk4Jqat+Ae26isxR3LeEyfaaQHfW0hokOuAMozYld7F16PQ8/&noverify=0',//嗽船激漂鹿-14
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=fAZMNLPsmMrb_ilkYL_XaPX3F91toTWb&authKey=t6FkgSXmdFtajTHfT0w4OYTCVIgUko6RGaL0uTjZFafh3U2tf80XS+V2sLNpmKWV&noverify=0',//只冒赖伙览-17
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=iu33aLTX7Z4Tiw0f07xw-a3uyjPT4uzD&authKey=2z66XdbBQsZLleYfGYOeeWJCdCZfq0mhag6zNa4uhFmYorNdH9q0ShHF37n/qnYM&noverify=0',//店氏植硬王-12
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=9S24TXyXs_oPH1lV5C7ra7_ScJ1iywOD&authKey=IRrpoydDGM3SbiItYQYWDns2uzT1rjWYUksCW2YYHnG2uXY8vCWh5M7QZHOHCedt&noverify=0',//逆纹凉摊罚-16
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=HNTHDWpSQK50g9XaKlWFzQ5Uyo6kK9RV&authKey=N2//m0aEQdeNs6N70/zdYFl1yaHu7fJcv14j5htLanqLg5rd5mgsK/c+GutKxVBS&noverify=0',//酱秆因喇似-8
+
+'https://qm.qq.com/cgi-bin/qm/qr?k=0m8v6IlQhSe9nTHC0MC2fUCCMoeqOXdg&authKey=SGPpKi66rgdqm8jFo3+LSaaKwIBxhBJfRBQa8Zi96f2p50/v/KDIpBz4pmawUwsL&noverify=0',//专合戴凯落-9
+
+
+
+    ];
+location.href = aa[Math.floor(Math.random()*aa.length)];
